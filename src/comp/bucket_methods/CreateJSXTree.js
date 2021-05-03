dee0518f5a46d064fdeb15adc40142158ad4eaa0
Perry Angelora <peamaillist@gmail.com>
2021-05-03 18:40:53 -0400

    Added logic to handle folder opening and closing.

--- src/comp/bucket_methods/CreateJSXTree.js
+++ src/comp/bucket_methods/CreateJSXTree.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { VscFile, VscFolder } from 'react-icons/vsc';
 import '../../style/main.scss';
 import DragDrop from './DragDrop';
@@ -14,28 +14,43 @@ let index = 0;
  * Method to render a JSX tree of a file directory
  * @param {Object[]} data Array of objects containing folder and file info
  * @return {JSX} JSX output
+ * pathState and setPathState are passed in from JsxPathHolder
  */
-export function CreateJSXTree({ data, bucket, setState, region }) {
+export function CreateJSXTree({
+  data, bucket, setState,
+  region, pathState, setPathState }) {
   return data.map((item) => {
     if (item.type === 'file') {
-      return <File
-        key={`${item.name}${++index}`}
-        name={item.name}
-        path={item.path}
-        region={region}
-        bucket={bucket}
-        setState={setState}/>;
+      return (
+        <File
+          key={`${item.name}${++index}`}
+          name={item.name}
+          path={item.path}
+          region={region}
+          bucket={bucket}
+          setState={setState}/>
+      );
     }
     if (item.type === 'folder') {
-      return (<Folder
-        key={`${item.name}${++index}`}
-        region={region}
-        bucket={bucket}
-        name={`${item.name}`}
-        path={item.path}
-        setState={setState}>
-        <CreateJSXTree data={item.children} bucket={bucket} region={region}/>
-      </Folder>);
+      return (
+        <Folder
+          key={`${item.name}${++index}`}
+          region={region}
+          bucket={bucket}
+          name={`${item.name}`}
+          path={item.path}
+          setState={setState}
+          pathState={pathState}
+          setPathState={setPathState}>
+          <CreateJSXTree
+            data={item.children}
+            bucket={bucket}
+            region={region}
+            setState={setState}
+            pathState={pathState}
+            setPathState={setPathState}
+          />
+        </Folder>);
     }
   });
 }
@@ -56,8 +71,8 @@ function File({ name, path, bucket, setState, region }) {
   return (
     <DragDrop bucket={bucket} path={path} setState={setState}>
       <li className={`jsx-li ${makeCssValid(bucket)}${path === undefined ?
-       '' :
-       '-' + makeCssValid(path)}`}
+        '' :
+        '-' + makeCssValid(path)}`}
       data-region={region}>
         <VscFile />
         <span className='file-text sm-text'>{name}</span>
@@ -79,13 +94,29 @@ File.propTypes = {
  * @param {array} children Children to render when opened
  * @return {JSX} Rendered JSX
  */
-function Folder({ name, children, bucket, path, setState, region }) {
-  const [folderOpen, setFolderOpen] = useState(false);
+function Folder({
+  name, children, bucket, path,
+  setState, region, pathState, setPathState }) {
+  const pathTest = (currentPath) => {
+    if (pathState.includes(currentPath)) {
+      // toggle open folders
+      return pathState.filter((e) => e !== currentPath);
+    }
+    // check to see if the folder being opened is in the path of an
+    // already opened folder
+    for (let i = pathState.length - 1; i >= 0; i--) {
+      if (currentPath.includes(pathState[i]) === false) {
+        pathState = pathState.filter((e) => e !== pathState[i]);
+      }
+    }
+    return [...pathState, currentPath];
+  };
 
   const handleToggle = (e) => {
     e.preventDefault();
     e.stopPropagation();
-    setFolderOpen(!folderOpen);
+    const newPath = pathTest(path);
+    setPathState(newPath);
   };
 
   return (
@@ -98,7 +129,8 @@ function Folder({ name, children, bucket, path, setState, region }) {
         <VscFolder className='folder-icon' />
         <span className='folder-text md-text'>{name}</span>
         <div
-          className={folderOpen ?
+          {...console.log(pathState, '+++++')}
+          className={pathState.includes(path) ?
             'folder-children-open' :
             'folder-children-close'}
         >{children}</div>
@@ -114,4 +146,6 @@ Folder.propTypes = {
   path: propTypes.string,
   setState: propTypes.func,
   region: propTypes.string,
+  pathState: propTypes.array,
+  setPathState: propTypes.func,
 };
