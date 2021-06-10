a60de0a4f47aa5e68e3ecb4ec1bb3ee971a17e3a
Perry Angelora <peamaillist@gmail.com>
2021-06-10 09:59:06 -0400

    Changes double quotes to single quotes

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,44 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 // const webpack = require("webpack");
 
 module.exports = {
-  entry: path.resolve(__dirname) + "/src/index.js",
+  entry: path.resolve(__dirname) + '/src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "index.bundle.js",
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.bundle.js',
     // publicpath: "/",
   },
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"],
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
       {
         test: /\.(js)$/,
-        use: "babel-loader",
+        use: 'babel-loader',
       },
       {
         test: /\.(woff(2)?|ttf|eot|svg|png)(\?v=\d+\.\d+\.\d+)?$/,
         use: [
           {
-            loader: "file-loader",
+            loader: 'file-loader',
             options: {
-              name: "[name].[ext]",
-              outputPath: "fonts/",
+              name: '[name].[ext]',
+              outputPath: 'fonts/',
             },
           },
         ],
       },
     ],
   },
-  plugins: [new HtmlWebpackPlugin({ template: "./src/index.html" })],
-  mode: process.env.NODE_ENV == "production" ? "production" : "development",
-    devServer: {
-      historyApiFallback: true,
-      inline:true,
-      port: 8080,
-      host: '192.168.1.152'
-    },
+  plugins: [new HtmlWebpackPlugin({ template: './src/index.html' })],
+  mode: process.env.NODE_ENV == 'production' ? 'production' : 'development',
+  devServer: {
+    historyApiFallback: true,
+    inline: true,
+    port: 8080,
+    host: '192.168.1.152',
+  },
 };
