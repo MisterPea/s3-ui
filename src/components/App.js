b9ff16d42d4fa993fe73b67ea07f338676e974aa
Perry Angelora <peamaillist@gmail.com>
2021-08-12 14:04:31 -0400

    Initial commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,30 @@
-import React from 'react'
+import * as React from 'react';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import MainLogo from './MainLogo';
+import BucketDisplay from './BucketDisplay';
+import { getBucketList } from '../actions/bucket';
+import '../style/main.scss';
 
-export default function App(){
-    return <div>Welcome!</div>
+/**
+ * Application entry point
+ * @return {JSX}
+ */
+export default function App() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(getBucketList());
+  }, [dispatch]);
+
+  return (
+    <>
+      <div className="main-wrapper">
+        <div className="nav-bar">
+          <div className="logo-lg"><MainLogo /></div>
+          <p className="contact-btn">Contact</p>
+        </div>
+        <BucketDisplay />
+      </div>
+    </>
+  );
 }
