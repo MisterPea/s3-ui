195a296f95b868726c6ad150b11f0afd56c0e130
Perry Angelora <peamaillist@gmail.com>
2021-10-04 09:23:09 -0400

    Folder reducer removed. Folder reducers have to be with bucket reducers

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -2,11 +2,9 @@ import { combineReducers } from 'redux';
 import buckets from './buckets';
 import error from './error';
 import loading from './loading';
-import folder from './folder';
 
 export default combineReducers({
   loading,
   error,
   buckets,
-  folder,
 });
