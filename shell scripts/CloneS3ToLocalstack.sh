5649225089339a3e113ef045f7b39290d63070be
Perry Angelora <peamaillist@gmail.com>
2021-10-03 22:40:16 -0400

    Added methods to create upload directory and delete after use; added variables for said directory

--- shell scripts/CloneS3ToLocalstack.sh
+++ shell scripts/CloneS3ToLocalstack.sh
@@ -1,7 +1,11 @@
-# This script clones all your s3 buckets to a local directory (in this case /Users/mothership/aws-s3-buckets) 
+# This script clones all your s3 buckets to a local directory (in this case /Users/mothership/Google Drive/JS/S3 FTP/shell scripts/aws-s3-buckets) 
 # and then populates the localstack s3 mock with the same files. This allows you to rebuild your localstack mock when needed.
-for bucket in $(aws s3api list-buckets --query "Buckets[].Name" --output text); do aws s3 sync s3://${bucket} /Users/mothership/aws-s3-buckets/${bucket}; done
+TEMP_DIR="/Users/mothership/Google Drive/JS/S3 FTP/shell scripts/aws-s3-buckets"
+mkdir "$TEMP_DIR"
 
-cd /Users/mothership/aws-s3-buckets/
+for bucket in $(aws s3api list-buckets --query "Buckets[].Name" --output text); do aws s3 sync s3://${bucket} "$TEMP_DIR"/${bucket}; done
+
+cd "$TEMP_DIR"/
  
 for bucket in $(ls); do aws s3 mb s3://${bucket} --endpoint-url http://localhost:4566 & aws s3 sync ./${bucket} --endpoint-url http://localhost:4566 s3://${bucket}; done
+rm -r "$TEMP_DIR"
