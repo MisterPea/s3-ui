c765a8ba467637d00b73cbecdaa2477488540183
Perry Angelora <peamaillist@gmail.com>
2021-10-04 09:20:45 -0400

    Added folder-centric routes

--- backend/routes.js
+++ backend/routes.js
@@ -7,9 +7,11 @@ const {
   createBucket,
   emptyBucket,
   getBucketContents,
+  getBucketContentsForDeletion,
   addFolder,
   deleteBucketContents,
   deleteBucket,
+  deleteFolder,
 } = require('./S3_topLevelMethods');
 
 router.get('/getBucketList', (req, res) => {
@@ -28,6 +30,15 @@ router.post('/getBucketContents', jsonParser, (req, res, next) => {
       res.send(data);
     })
     .catch((err) => next(`ERROR in server-retrieving bucket contents: ${err}`));
+});
+
+router.post('/getBucketContentsForDeletion', jsonParser, (req, res, next) => {
+  const { locale, bucket } = req.body;
+  getBucketContentsForDeletion(locale, bucket)
+    .then((data) => {
+      res.send(data);
+    })
+    .catch((err) => next(`ERROR in getting contents for deletion: ${err}`));
 });
 
 router.post('/getBucketsAndContents', jsonParser, (req, res, next) => {
@@ -58,8 +69,20 @@ router.post('/createBucket', jsonParser, (req, res, next) => {
 // ERROR in server-retrieving buckets in (BucketsAndContents): Error: TimeoutError: connect ETIMEDOUT 172.19.1.1:443: Getting buckets
 
 router.post('/createFolder', jsonParser, (req, res, next) => {
-  const { locale, folderPath, bucket } = req.body;
-  addFolder(locale, folderPath, bucket)
+  const {
+    locale, folderPath, bucket, folderName,
+  } = req.body;
+  addFolder(locale, folderPath, bucket, folderName)
+    .then((data) => {
+      res.status(200).send(data);
+    }).catch((err) => next(err));
+});
+
+router.post('/deleteFolder', jsonParser, (req, res, next) => {
+  const {
+    locale, bucket, pathToDelete, folderName,
+  } = req.body;
+  deleteFolder(locale, bucket, pathToDelete, folderName)
     .then((data) => {
       res.status(200).send(data);
     }).catch((err) => next(err));
