8fe6f796e3da57620c282fefe3ca5df154f909f8
Perry Angelora <peamaillist@gmail.com>
2021-10-04 09:38:08 -0400

    Updated naming

--- frontend/tests/ListItem.test.js
+++ frontend/tests/ListItem.test.js
@@ -3,19 +3,19 @@ import ReactDOM from 'react-dom';
 import toJson from 'enzyme-to-json';
 import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import ListItem from '../src/components/ListItem';
+import FileLI from '../src/components/FileLI';
 
 configure({ adapter: new Adapter() });
 
-describe('ListItem component', () => {
+describe('FileLI component', () => {
   it('Renders without crashing', () => {
     const div = document.createElement('DIV');
-    ReactDOM.render(<ListItem type={'folder'} name={'empty folder'}/>, div);
+    ReactDOM.render(<FileLI type={'folder'} name={'empty folder'}/>, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   it('Renders the folder info passed to it', () => {
-    const wrapper = shallow(<ListItem type={'folder'} name={'empty folder'}/>);
+    const wrapper = shallow(<FileLI type={'folder'} name={'empty folder'}/>);
     expect(toJson(wrapper)).toMatchSnapshot();
   });
 });
