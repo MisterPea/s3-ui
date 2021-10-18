6a6615d8bc62838dd2a27c0ba2525175f0f12c28
Perry Angelora <peamaillist@gmail.com>
2021-10-17 21:47:57 -0400

    Changed tooltip file/folder options to dropdown/accordian interaction

--- frontend/src/components/FileLI.js
+++ frontend/src/components/FileLI.js
@@ -1,11 +1,10 @@
 import * as React from 'react';
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 import propTypes from 'prop-types';
 import { IoEllipsisVerticalCircle } from 'react-icons/io5';
-import { AnimatePresence, motion } from 'framer-motion';
 import { Folder } from './graphic_elements/Icons';
 import FileIcon from './helpers/FileIcon';
-import FolderTooltip from './FolderTooltip';
+import FolderDropdown from './FolderDropdown';
 import FolderDeleteModal from './FolderDeleteModal';
 import FolderUploadModal from './FolderUploadModal';
 import ModalComponentWrapper from './ModalComponentWrapper';
@@ -18,7 +17,7 @@ import ModalComponentWrapper from './ModalComponentWrapper';
  * @prop {number} size Size of the file (Only for files)
  * @prop {string} filePath File path leading to children (Only for folders)
  * @prop {func?} folderClick Callback for folders that are clicked
- * @prop {func} openOptions Callback for clicks to ellipsis button
+ * @prop {string} locale Region of the bucker - passed to modal.
  * @return {JSX}
  */
 export default function FileLI({
@@ -27,6 +26,7 @@ export default function FileLI({
   const [toggleTooltip, setToggleTooltip] = useState(false);
   const [toggleDeleteModal, setToggleDeleteModal] = useState(false);
   const [toggleUploadModal, setToggleUploadModal] = useState(false);
+  const isOpenRef = useRef(false);
 
   function onKeyDownFolder(e) {
     e.preventDefault();
@@ -49,81 +49,42 @@ export default function FileLI({
     setToggleUploadModal((s) => !s);
   }
 
-  const tooltipVariants = {
-    open: {
-      opacity: 1,
-      transitionEnd: { zIndex: 10100 },
-      scale: 1,
-      x: -3,
-      y: 2,
-      transition: {
-        type: 'tween',
-        duration: 0.2,
-        ease: [0.06, 0.83, 0.37, 1],
-      },
-
-    },
-    closed: {
-      opacity: 0,
-      transitionEnd: { zIndex: 10000 },
-      scale: 0.95,
-      x: 5,
-    },
-    exit: {
-      scaleY: 0.7,
-      scaleX: 0.3,
-      opacity: 0,
-      transitionEnd: { zIndex: 10000 },
-      transition: {
-        type: 'tween',
-        duration: 0.1,
-        ease: [0.57, 0.24, 0.74, 0.86],
-      },
-    },
-  };
-
   if (type === 'folder') {
     return (
-      <div
-        className="file-row"
-      >
-        <div
-          role="button"
-          tabIndex={0}
-          onClick={() => folderClick(filePath)}
-          onKeyDown={(e) => onKeyDownFolder(e)}
-          className="file-row-left"
-        >
-          <div className="icon-wrapper"><Folder /></div>
-          <p className="file-table-data">{name}</p>
-        </div>
-        <IoEllipsisVerticalCircle
-          className="file-row-right"
-          onClick={handleToggleTooltip}
-        />
-        <AnimatePresence>
-          {toggleTooltip && (
-          <motion.div
-            variants={tooltipVariants}
-            initial="closed"
-            animate={toggleTooltip ? 'open' : 'closed'}
-            key={name}
-            exit="exit"
+      <div className="file-row-wrapper">
+        <div className="file-row">
+          <div
+            role="button"
+            tabIndex={0}
+            onClick={() => folderClick(filePath)}
+            onKeyDown={(e) => onKeyDownFolder(e)}
+            className="file-row-left"
           >
-            <FolderTooltip
-              buttonClick={handleCloseTooltip}
-              closeTooltip={handleToggleTooltip}
-              toggleDeleteModal={handleDeleteModal}
-              toggleUploadModal={handleUploadModal}
-            />
-          </motion.div>
-
-          )}
-        </AnimatePresence>
+            <div className="icon-wrapper"><Folder /></div>
+            <p className="file-table-data">{name}</p>
+          </div>
+          <IoEllipsisVerticalCircle
+            className="file-row-right"
+            onClick={handleToggleTooltip}
+          />
+        </div>
+        <FolderDropdown
+          isOpen={toggleTooltip}
+          isOpenRef={isOpenRef}
+          toggleDeleteModal={handleDeleteModal}
+          toggleUploadModal={handleUploadModal}
+          closeDropdown={handleToggleTooltip}
+          buttonClick={handleCloseTooltip}
+        />
         { toggleDeleteModal
     && (
     <ModalComponentWrapper close={handleDeleteModal}>
-      <FolderDeleteModal locale={locale} bucket={bucket} pathToDelete={filePath} folderName={name} />
+      <FolderDeleteModal
+        locale={locale}
+        bucket={bucket}
+        pathToDelete={filePath}
+        folderName={name}
+      />
     </ModalComponentWrapper>
     ) }
         { toggleUploadModal
@@ -136,22 +97,24 @@ export default function FileLI({
     );
   }
   return (
-    <div
-      className="file-row"
-    >
+    <div className="file-row-wrapper">
       <div
-        className="file-row-left"
-        role="button"
-        tabIndex={0}
+        className="file-row"
       >
-        <div className="icon-wrapper"><FileIcon name={name} /></div>
-        <p className="file-table-data name">{name}</p>
-        <p className="file-table-data last-modified">{lastModified}</p>
-        <p className="file-table-data size">{size}</p>
+        <div
+          className="file-row-left"
+          role="button"
+          tabIndex={0}
+        >
+          <div className="icon-wrapper"><FileIcon name={name} /></div>
+          <p className="file-table-data name">{name}</p>
+          <p className="file-table-data last-modified">{lastModified}</p>
+          <p className="file-table-data size">{size}</p>
+        </div>
+        <IoEllipsisVerticalCircle
+          className="file-row-right"
+        />
       </div>
-      <IoEllipsisVerticalCircle
-        className="file-row-right"
-      />
     </div>
   );
 }
