0c90503b473d0dc5b25901d605806da380dbd53f
Perry Angelora <peamaillist@gmail.com>
2021-10-17 21:51:58 -0400

    Added useScrollIntersect hook to add box-shadows when content is under header/footer

--- frontend/src/components/BucketDisplay.js
+++ frontend/src/components/BucketDisplay.js
@@ -8,15 +8,18 @@ import { getBucketList } from '../redux/actions/bucket';
 import ModalComponentWrapper from './ModalComponentWrapper';
 import AddBucketModal from './AddBucketModal';
 import BucketUL from './BucketUL';
+import useScrollIntersect from './helpers/useScrollIntersect';
 
 export default function BucketDisplay() {
   const { loading, buckets } = useSelector((state) => state);
   const [addBucketModal, setAddBucketModal] = useState(false);
-
+  const [[scrollTarget], setScrollTarget] = useState([null]);
   const dispatch = useDispatch();
   useEffect(() => {
     dispatch(getBucketList());
   }, []);
+
+  const [topScrollShadow, bottomScrollShadow, handleScroll] = useScrollIntersect(buckets, scrollTarget);
 
   function handleToggleModal() {
     setAddBucketModal((s) => !s);
@@ -57,12 +60,13 @@ export default function BucketDisplay() {
   };
 
   return (
-    <div>
-      <div className="buckets-number">{`Buckets (${buckets ? buckets.length : '0'})`}</div>
-      <div className="table-head bucket-display">
-        <h3 className="name-header">Bucket Name</h3>
-        <h3 className="date-header">Creation Date</h3>
-        <h3 className="region-header">AWS Region</h3>
+    <div className="bucket-display-wrapper">
+      <div className="bucket-header-wrapper">
+        <div className={`bucket-display ${topScrollShadow ? 'overflow' : ''}`}>
+          <h3 className="bucket-name-header">Bucket Name</h3>
+          <h3 className="bucket-date-header">Creation Date</h3>
+          <h3 className="bucket-region-header">AWS Region</h3>
+        </div>
       </div>
       <AnimatePresence exitBeforeEnter>
         {loading && (
@@ -71,13 +75,15 @@ export default function BucketDisplay() {
           </motion.div>
         )}
         <motion.div
+          onScroll={(e) => handleScroll(e)}
+          className="ul-wrapper"
           key="bucketUL"
           variants={bucketVariant}
           animate="open"
           initial="closed"
           exit="closed"
         >
-          <BucketUL buckets={buckets} loading={!loading} />
+          <BucketUL buckets={buckets} loading={!loading} onFinish={setScrollTarget} />
         </motion.div>
       </AnimatePresence>
 
@@ -86,7 +92,7 @@ export default function BucketDisplay() {
         onKeyPress={(e) => handleKeyPress(e)}
         role="button"
         tabIndex={0}
-        className="add-bucket-bar"
+        className={`add-bucket-bar${bottomScrollShadow ? ' overflow' : ''}`}
       >
         <span className="bucket-button-elements">
           <div className="bucket-cta-wrapper">
