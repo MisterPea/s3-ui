b74671fd8182ab2d7aab407f5fca2d18f1074d3a
Perry Angelora <peamaillist@gmail.com>
2021-10-26 18:28:58 -0400

    Added axios call to download file

--- frontend/src/components/FileModal.js
+++ frontend/src/components/FileModal.js
@@ -1,12 +1,31 @@
 import * as React from 'react';
-import { IoCloudDownloadOutline, IoTrashOutline } from 'react-icons/io5';
+import { IoCloudDownloadOutline, IoTrashOutline, IoInformationCircleSharp } from 'react-icons/io5';
 import propTypes from 'prop-types';
+import axios from 'axios';
 import fileSizeTruncate from './helpers/fileSizeTruncate';
 
-export default function FileModal({ fileInfo, setModalOpen }) {
+export default function FileModal({ fileInfo, setModalOpen, downloadInfo }) {
   const { name, lastModified, size } = fileInfo;
+  const { hostname } = window.location;
 
   function handleDownloadClick() {
+    const { locale, bucket, filePath } = downloadInfo;
+    const key = `${(filePath && filePath.slice(1)) || ''}/${name}`;
+    axios({
+      method: 'POST',
+      url: `http://${hostname}:3200/downloadFile`,
+      data: { locale, bucket, key },
+      headers: {
+        'content-type': 'application/json',
+      },
+    }).then((url) => {
+      const link = document.createElement('A');
+      link.setAttribute('download', name);
+      link.href = url.data;
+      document.body.appendChild(link);
+      link.click();
+      link.remove();
+    });
     setModalOpen();
   }
 
@@ -14,33 +33,65 @@ export default function FileModal({ fileInfo, setModalOpen }) {
     setModalOpen();
   }
 
+  function toDate() {
+    return lastModified.split('T')[0];
+  }
+
   return (
     <>
-      <div>
-        <h3>File Info:</h3>
-        <p>{`Name: ${name}`}</p>
-        <p>{`Size: ${fileSizeTruncate(size)}`}</p>
-        <p>{`Last Modified: ${lastModified}`}</p>
-      </div>
-      <div className="file-button-wrapper">
-        <button
-          type="button"
-          onClick={handleDownloadClick}
-        >
-          <div className="file-icon"><IoCloudDownloadOutline /></div>
-          <p>Download File</p>
-        </button>
-        <button
-          type="button"
-          onClick={handleDeleteClick}
-        >
-          <div className="file-icon"><IoTrashOutline /></div>
-          <p>Delete File</p>
-        </button>
+      <div className="modal-wrapper">
+        <div className="modal-button-group">
+          <header className="modal-header">
+            <IoInformationCircleSharp />
+            <h3>FILE INFO:</h3>
+          </header>
+          <div className="file-info-body">
+            <div className="file-info">
+              <h4>NAME:</h4>
+              <p>{` ${name}`}</p>
+            </div>
+            <div className="file-info">
+              <h4>SIZE:</h4>
+              <p>{` ${fileSizeTruncate(size)}`}</p>
+            </div>
+            <div className="file-info">
+              <h4>LAST MODIFIED:</h4>
+              <p>{` ${toDate()}`}</p>
+            </div>
+          </div>
+        </div>
+        <div className="file-button-wrapper">
+
+          <button
+            type="button"
+            onClick={handleDownloadClick}
+          >
+            <div className="button-content-wrapper">
+              <div className="file-icon"><IoCloudDownloadOutline /></div>
+              <h2>DOWNLOAD FILE</h2>
+            </div>
+          </button>
+          <button
+            type="button"
+            onClick={handleDeleteClick}
+          >
+            <div className="button-content-wrapper">
+              <div className="file-icon delete"><IoTrashOutline /></div>
+              <h2>DELETE FILE</h2>
+            </div>
+          </button>
+        </div>
       </div>
     </>
   );
 }
+FileModal.defaultProps = {
+  downloadInfo: {
+    locale: 'us-east-1',
+    bucket: '',
+    filePath: null,
+  },
+};
 
 FileModal.propTypes = {
   fileInfo: propTypes.shape({
@@ -50,4 +101,9 @@ FileModal.propTypes = {
   }).isRequired,
   // eslint-disable-next-line react/require-default-props
   setModalOpen: propTypes.func,
+  downloadInfo: propTypes.shape({
+    locale: propTypes.string,
+    bucket: propTypes.string,
+    filePath: propTypes.string,
+  }),
 };
