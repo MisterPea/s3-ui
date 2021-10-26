6e71e5b7a74197a96b8cf81f1ebea9a51d054ba9
Perry Angelora <peamaillist@gmail.com>
2021-10-26 18:34:51 -0400

    Added one error case

--- frontend/src/redux/reducers/error.js
+++ frontend/src/redux/reducers/error.js
@@ -1,8 +1,9 @@
-import { ERR_GETTING_BUCKET_CONTENTS } from '../actions/error';
+import { ERR_GETTING_BUCKET_CONTENTS, ERR_DELETING_BUCKET } from '../actions/error';
 
 export default function error(state = null, action) {
   switch (action.type) {
     case ERR_GETTING_BUCKET_CONTENTS:
+    case ERR_DELETING_BUCKET:
       return action.message;
     default:
       return state;
