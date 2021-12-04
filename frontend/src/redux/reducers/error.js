44016135611185e64be4a852fc6b822aac1b8807
Perry Angelora <peamaillist@gmail.com>
2021-12-04 12:26:28 -0500

    Addeded missing errors and error reset

--- frontend/src/redux/reducers/error.js
+++ frontend/src/redux/reducers/error.js
@@ -1,9 +1,27 @@
-import { ERR_GETTING_BUCKET_CONTENTS, ERR_DELETING_BUCKET } from '../actions/error';
+import {
+  ERR_GETTING_BUCKET_CONTENTS,
+  ERR_GETTING_BUCKETS,
+  ERR_DELETING_BUCKET,
+  ERR_CREATING_BUCKET,
+  ERR_CREATING_FOLDER,
+  ERR_DELETING_FOLDER,
+  ERR_DOWNLOADING_FILE,
+  ERR_DELETING_FILE,
+  RESET_ERROR,
+} from '../actions/error';
 
 export default function error(state = null, action) {
   switch (action.type) {
     case ERR_GETTING_BUCKET_CONTENTS:
     case ERR_DELETING_BUCKET:
+    case ERR_GETTING_BUCKETS:
+    case ERR_CREATING_BUCKET:
+    case ERR_CREATING_FOLDER:
+    case ERR_DELETING_FILE:
+    case ERR_DELETING_FOLDER:
+    case ERR_DOWNLOADING_FILE:
+      return action.message;
+    case RESET_ERROR:
       return action.message;
     default:
       return state;
