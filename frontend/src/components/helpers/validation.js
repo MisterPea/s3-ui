68d2d4ce9d8722565ea79c655a6cc3c31c16a8e8
Perry Angelora <peamaillist@gmail.com>
2021-12-06 11:30:25 -0500

    Added catches for duplicate bucket names and folder names

--- frontend/src/components/helpers/validation.js
+++ frontend/src/components/helpers/validation.js
@@ -4,18 +4,31 @@
 //   dots (.), and hyphens (-).
 // • Bucket name must begin and end with a letter or number.
 // • Bucket name must not be formatted as an IP address.
-export function validateBucketName(name) {
+/**
+* @param {string} name Name of bucket to be added
+* @param {Array<string>} buckets Array of current bucket names
+* @return {boolean} Returns true if formatting is valid; false if it's invalid
+*/
+export function validateBucketName(name, buckets) {
+  const bucketExists = buckets.indexOf(name) === -1;
   const lengthAlphaTest = '^[a-z0-9][a-z0-9.-]{1,1023}[a-z0-9]$';
   const ipFormatTest = '^((?!(^([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})\.([0-9]{1,3})$)).)*$';
   const lengthValid = new RegExp(lengthAlphaTest);
   const ipValid = new RegExp(ipFormatTest);
-  return lengthValid.test(name) && ipValid.test(name);
+  return lengthValid.test(name) && ipValid.test(name) && bucketExists;
 }
 
-export function validateFolderName(name) {
+/**
+ * Validator for S3 objects (folders)
+ * @param {string} name Name of the folder to be added
+ * @param {Array<string>} folders Array of current folder names
+ * @return {boolean} Returns true if formatting is valid; false if it's invalid
+ */
+export function validateFolderName(name, folders) {
+  const folderExists = folders.indexOf(name) === -1;
   const folderNameTest = "^[A-Za-z0-9-()!_']{1,1023}$";
   const foldernameValid = new RegExp(folderNameTest);
-  return foldernameValid.test(name);
+  return foldernameValid.test(name) && folderExists;
 }
 
 /**
