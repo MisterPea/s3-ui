7233967d86a76023c426f87c3c65a529cc10add7
Perry Angelora <peamaillist@gmail.com>
2021-12-12 17:41:08 -0500

    Added uploadProgress to the reducer index

--- frontend/src/redux/reducers/index.js
+++ frontend/src/redux/reducers/index.js
@@ -2,9 +2,11 @@ import { combineReducers } from 'redux';
 import buckets from './buckets';
 import error from './error';
 import loading from './loading';
+import uploadProgress from './uploadProgress';
 
 export default combineReducers({
   loading,
   error,
   buckets,
+  uploadProgress,
 });
