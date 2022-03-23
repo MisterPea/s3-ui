9f9e1a5f4c6c59ed80128353966287dac2e92f6a
Perry Angelora <peamaillist@gmail.com>
2022-03-23 12:02:30 -0400

    Resolve erronious env variable

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -45,7 +45,7 @@ module.exports = {
     new HtmlWebpackPlugin({ template: './src/index.html' }),
     new DefinePlugin({
       HOSTNAME: process.env.NODE_ENV === 'production' ? JSON.stringify('s3ui.misterpea.me') : JSON.stringify('192.168.1.152:5001'),
-      SSL: process.env.SECURE === true ? JSON.stringify('https://') : JSON.stringify('http://'),
+      SSL: process.env.PREFIX,
     }),
   ],
   mode: process.env.NODE_ENV === 'production' ? 'production' : 'development',
